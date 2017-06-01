c92e5ac7557e4f79d283b9f155bf34ed56b4e74b
pipi9baby <pipi9baby@gmail.com>
2017-06-01 23:08:53 +0800

    GA那個更新，但是還沒有實作出突變的部分

--- HW4_GA_eightqueen/gn_eightQueen.c
+++ HW4_GA_eightqueen/gn_eightQueen.c
@@ -5,18 +5,10 @@
 #include<limits.h>
 
 int size = 8,value=0;
-int *curr_arr;
 int *next_arr;
 int *father;
 int *mother;
-//次方
-int pow(int a,int b){ 
-	int n=1,x=0; 
-	for(x=0;x<b;x++){ 
-		n=n*a;
-	}
-	return n; 
-} 
+ 
 //計算適應值
 int colision(int arr[]){
 	int i=0,j=0,num=0,offset=0;
@@ -43,7 +35,6 @@ void swap(int A,int B){
 void merge(){
 	int cut = rand()%size;
 	int i=0;
-	printf("\n");
 	for(i=0;i<cut;i++){
 		swap(father[i],mother[i]);
 	}
@@ -93,6 +84,10 @@ void choseFM(int arr[],int N){
 			}
 		}
 	}
+	for(i=0;i<N;i++){
+		total_col += cols_num[i][2];
+	}
+	//printf("%d\n",total_col);
 	int stF;
 	Fran = rand()%total_col;
 	//printf("亂數 %d,total_col %d\n",Fran,total_col);
@@ -131,24 +126,28 @@ void choseFM(int arr[],int N){
 //主程式
 int main() {
 
-	int prob=0,N=0;
+	int prob=0,N=0,loop=0;
 	char input[5];
 	
 	//輸入參數
 	printf("請輸入棋盤大小(純數字)：");
 	gets(input);
 	size = atoi(input);
-	curr_arr = (int*)malloc(sizeof(int)*size);
 	next_arr = (int*)malloc(sizeof(int)*size);
 	father = (int*)malloc(sizeof(int)*size);
 	mother = (int*)malloc(sizeof(int)*size);
+	
+	printf("請輸入下一代群體的個數：");
+	gets(input);
+	N = atoi(input);
+	
 	printf("請輸入突變的機率為多少分之一：");
 	gets(input);
 	prob = atoi(input);
 	
-	printf("請輸入一代群體的個數：");
+	printf("請輸入要求幾個解：");
 	gets(input);
-	N = atoi(input);
+	loop = atoi(input);
 	
 	//計算最高適應值
 	int i=0,j=0,success=0,st_col=0;
@@ -161,55 +160,47 @@ int main() {
     seed = (unsigned)time(NULL); // 取得時間序列
     srand(seed); // 以時間序列當亂數種子	
 	
-	printf("\n");
-	//亂數皇后的初始位置
-	for(j=0;j<size;j++){
-		curr_arr[j] = rand()%size;
-		next_arr[j] = 0;
-		printf("%d ",curr_arr[j]);
-	}
-	printf("\n");
-	
-	
-	//開始基因演算法
-	while(1){
-		int col=0,count=0;
-		choseFM(curr_arr,N);
-		
-		merge();
-		int colF = colision(father);
-		int colM = colision(mother);
-
-		if(colF>colM){
-			for(j=0;j<size;j++){
-				next_arr[j] = father[j];
-			}
-			col = colF;
-		}else{
-			for(j=0;j<size;j++){
-				next_arr[j] = mother[j];
-			}
-			col = colM;
-		}
-		
-		//printf("分數：%d\n",colision(next_arr));
-		if(colision(next_arr) == value){
-			break;
-		}
+	while(loop!=0){
+		//亂數皇后的初始位置
+		printf("\n亂數起始值：\n");
 		for(j=0;j<size;j++){
-			curr_arr[j] = next_arr[j];
-			//printf("%d\t",curr_arr[j]);
+			next_arr[j] = rand()%size;
+			printf("%d\t",next_arr[j]);
 		}
-		//printf("\n");
-		//system("PAUSE");
-	}
-	
-	//print棋盤
-	if(colision(next_arr) == value){
+		printf("\n");
+	//開始基因演算法
+		while(1){
+			int col=0,count=0;
+			choseFM(next_arr,N);
+			
+			merge();
+			int colF = colision(father);
+			int colM = colision(mother);
+
+			if(colF>colM){
+				for(j=0;j<size;j++){
+					next_arr[j] = father[j];
+				}
+				col = colF;
+			}else{
+				for(j=0;j<size;j++){
+					next_arr[j] = mother[j];
+				}
+				col = colM;
+			}
+			
+			//printf("分數：%d\n",colision(next_arr));
+			if(colision(next_arr) == value){
+				break;
+			}
+			//system("PAUSE");
+		}
+		
+		//print棋盤
 		success++;
 		printf("第%d次\n",success);
 		for(j=0;j<size;j++){
-			printf("%d\t",curr_arr[j]);
+			printf("%d\t",next_arr[j]);
 		}
 		printf("\n+");
 		for(j=0;j<size;j++){
@@ -220,7 +211,7 @@ int main() {
 		for(k=0;k<size;k++){
 			printf("|");
 			for(j=0;j<size;j++){
-				if(j == curr_arr[k]){
+				if(j == next_arr[k]){
 					printf(" Q |");
 				}else{
 					printf("   |");
@@ -242,7 +233,7 @@ int main() {
 			int k=0;
 			for(k=0;k<size;k++){
 				for(j=0;j<size;j++){
-					if(curr_arr[k] == j){
+					if(next_arr[k] == j){
 						fprintf(wFile,"1");
 					}else{
 						fprintf(wFile,"0");
@@ -258,6 +249,7 @@ int main() {
 			//fwrite(enter,1,sizeof(enter),wFile);
 		}
 		fclose(wFile);
+		loop--;
 	}
 	//*/
 	system("PAUSE");
